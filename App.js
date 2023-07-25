136523377d8de384c4707630668f6291b0c1f833
githmiDeSilva <140400074+githmiDeSilva@users.noreply.github.com>
2023-07-25 19:09:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import AddStudent from './components/AddStudent';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+  
+      <div>
+        <Header />
+        <AddStudent  />
+      </div>
+ 
+  );
+}
+
+export default App;
+
